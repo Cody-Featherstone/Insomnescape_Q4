82a8de60737ebd57b52bbd253a4e491febafe15b
Cody-Featherstone <Cfeatherstone1002@gmail.com>
2021-04-29 08:32:17 -0700

    Changed the AI script to Have line of sight and patrol.

--- Assets/CodyFeatherstone/Enemy.cs
+++ Assets/CodyFeatherstone/Enemy.cs
@@ -8,8 +8,20 @@ public class Enemy : MonoBehaviour
     public float startWaitTime;
     private float waitTime;
 
-    public Transform[] moveSpots;
-    private int randomSpots;
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    Rigidbody2D rb;
+
+
+
+    public float distance;
+    public float rotationSpeed;
+
+    public LineRenderer lineOfSight;
 
 
 
@@ -17,25 +29,55 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         waitTime = startWaitTime;
-        randomSpots = Random.Range(0, moveSpots.Length);
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+
+        //Spotting Mechanic
+        Physics2D.queriesStartInColliders = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpots].position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
 
-        if(Vector2.Distance(transform.position, moveSpots[randomSpots].position) < 0.2f)
+        if(Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
         {
             if(waitTime<= 0)
             {
-                randomSpots = Random.Range(0, moveSpots.Length);
+                
                 waitTime = startWaitTime;
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
             }
             else
             {
                 waitTime -= Time.deltaTime;
             }
         }
+        //the Rotation and detection of the player and LOS
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if(hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            lineOfSight.SetPosition(1, hitInfo.point);
+
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+
+        }
+        else
+        {
+
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+            lineOfSight.SetPosition(1, transform.position + transform.right * distance);
+        }
+
+        lineOfSight.SetPosition(0, transform.position);
     }
+   
 }
