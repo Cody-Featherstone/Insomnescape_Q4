53dd106b3d188f7c85c4e03a4414b83f6f8fc7ac
1703959 <1703959@SC-VDGD-6QG6853.local>
2021-04-29 09:59:50 -0700

    Finished hiding

---
+++ Assets/ColtCandland/Hiding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiding : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "MainCharacter")
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
