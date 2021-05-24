a22d2ef30f35bbd04e262e28571a7c9eb3664b2f
Cody-Featherstone <Cfeatherstone1002@gmail.com>
2021-05-24 09:33:01 -0700

    Made a New Script for the Key Pickup and a win condiction.

---
+++ Assets/CodyFeatherstone/Scripts/KeyPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyPickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GameManager.KeyCount++;
+        }
+        if(GameManager.KeyCount == 5)
+        {
+            //switch scenes
+            SceneManager.LoadScene("");
+            Debug.Log("YOU WIN!!");
+        }
+    }
+}
