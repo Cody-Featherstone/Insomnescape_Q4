9e335762b45aea9538a0b84c46d0f3d744cd7fda
Cody-Featherstone <Cfeatherstone1002@gmail.com>
2021-05-25 12:38:41 -0700

    Made New player and changed enemies a bit

--- Assets/CodyFeatherstone/Scripts/KeyPickUp.cs
+++ Assets/CodyFeatherstone/Scripts/KeyPickUp.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class KeyPickUp : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +15,21 @@ public class KeyPickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            GameManager.KeyCount++;
-        }
+        
         if(GameManager.KeyCount == 5)
         {
             //switch scenes
             SceneManager.LoadScene("Credits");
             Debug.Log("YOU WIN!!");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            GameManager.KeyCount++;
+            gameObject.SetActive(false);
         }
     }
 }
