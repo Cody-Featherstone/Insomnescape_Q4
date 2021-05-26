d6b330c8c49312563dafdf2430fd02bf1e1395b7
Cody-Featherstone <Cfeatherstone1002@gmail.com>
2021-05-26 09:47:13 -0700

    Made THe death condition

---
+++ Assets/EnemyKill.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyKill : MonoBehaviour
+{
+    public Transform Target;
+    private float Distance;
+    public GameObject Enemy;
+    private float TimeStamp;
+    private bool ClockIsTicking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(Enemy.transform.position, Target.transform.position) <= 0.02f)
+        {
+            if (!ClockIsTicking)
+            {
+                TimeStamp = Time.time;
+                ClockIsTicking = true;
+            }
+            else
+            {
+                if (Time.time - TimeStamp > 5)
+                {
+                    SceneManager.LoadScene("GameOver");
+                }
+            }
+
+
+        }
+        else
+        {
+            ClockIsTicking = false;
+        }
+    }
+}
