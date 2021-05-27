2aa185edbb1fa1a43bafc8639896414d1d3c7300
Colt Candland <Coltcandland03@gmail.com>
2021-05-26 17:15:40 -0700

    done

--- Assets/ColtCandland/Scripts/DontDestroy.cs
+++ Assets/ColtCandland/Scripts/DontDestroy.cs
@@ -5,37 +5,16 @@ using System;
 
 public class DontDestroy : MonoBehaviour
 {
-    private static DontDestroy _instance = null;
-
-    public static DontDestroy Instance
-    {
-        get { return _instance; }
-    }
-    
-    public static int sceneToDestroyObj = 0; //the index of the scene you want the object to Destroy on in your Build Settings
-    public static int sceneToDestroyObj1 = 3; //the index of the scene you want the object to Destroy on in your Build Settings
+    AudioSource src = null;
 
     void Awake()
     {
-        if (_instance != null && _instance != this)
-        {
-            Destroy(transform.root.gameObject);
-            return;
-        }
-        _instance = this;
-        DontDestroyOnLoad(transform.root.gameObject);
-
-        SceneManager.activeSceneChanged += DestroyOnMenuScreen;
+        src = GetComponent<AudioSource>();
+        src.time = PlayerPrefs.GetFloat("SongPoint");
     }
 
-    void DestroyOnMenuScreen(Scene oldScene, Scene newScene)
+    void OnDestroy()
     {
-        if (newScene.buildIndex == sceneToDestroyObj || newScene.buildIndex == sceneToDestroyObj1) //could compare Scene.name instead
-        {
-            Destroy(transform.root.gameObject); //change as appropriate
-        }
+        PlayerPrefs.SetFloat("SongPoint", src.time);
     }
-
-   
-
 }
